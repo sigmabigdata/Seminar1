16c9b1fbd441af2e9e7ae919140f059216cd9e6d
sigmabigdata <sigmabigdata@gmail.com>
2023-01-14 19:42:06 +0300

    Tak1 complit

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+int number1 = 5;
+int number2 = 10;
+
+if (number1 > number2)
+{
+    Console.WriteLine("Число " + number1 + " больше числа " + number2);
+} 
+else if (number1 < number2)
+{
+    Console.WriteLine($"Число " + number1 + " меньше числа " + number2);
+}
+
+else
+Console.WriteLine($"Число " + number1 + " равно числу " + number2);
